bd9734649f7b91154d01f32af1cc51ec83069c8b
spasevska <spasevska.dijana@gmail.com>
2021-05-03 22:00:02 +0200

    first commit

---
+++ Proba/ProbaPrva.java
@@ -0,0 +1,20 @@
+package Proba;
+
+public class ProbaPrva {
+	private int age = 12;
+	public String name = "Emma";
+	public String type = "dog";
+	
+	public void eat() {
+		System.out.println("Java");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Java");
+		String name = "Dijana";
+		System.out.println("Hi from " + name);
+		System.out.println(args[1]);
+		System.out.println(args[4]);
+	}
+
+}
---
+++ Proba/ProbaVtora.java
@@ -0,0 +1,11 @@
+package Proba;
+
+public class ProbaVtora {
+
+	public static void main(String[] args) {
+		ProbaPrva a1 = new ProbaPrva();
+		a1.eat();
+		System.out.println(a1.name);
+	}
+
+}
